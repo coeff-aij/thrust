5c5f6285df0e046e5e5e27f1db2f4c65f6b73365
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-02-02 17:10:27 +0900

    add: test for Range Iterator implementation using trait

---
+++ tests/ui/pass/range_iterator.rs
@@ -0,0 +1,80 @@
+//@check-pass
+//@compile-flags: -C debug-assertions=off
+//@rustc-env: THRUST_SOLVER=thrust-pcsat-wrapper
+
+trait Iterator {
+    type Item;
+    
+    #[thrust::requires(true)]
+    #[thrust::ensures(
+        (completed(*self) || (exists i:int. (result == std::option::Option::<int>::Some(i)) && step(*self, i, ^self)))
+        && (!completed(*self) || (result == std::option::Option::<int>::None() && *self == ^self))
+    )]
+    fn next(&mut self) -> Option<Self::Item>;
+    
+    #[thrust::predicate]
+    fn completed(self) -> bool;
+    #[thrust::predicate]
+    fn step(self, item: Self::Item, dist: Self) -> bool;
+}
+
+struct Range {
+    start: i64,
+    end: i64,
+}
+
+
+
+impl Iterator for Range {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start < self.end {
+            let item = self.start;
+            self.start += 1;
+            Some(item)
+        } else {
+            None
+        }
+    }
+    
+    #[thrust::predicate]
+    fn completed(self) -> bool {
+        // (tuple_proj<Int-Int>.0 self) is equivalent to self.start
+        // self.start < self.end is written as following:
+        "(not (<
+            (tuple_proj<Int-Int>.0 self)
+            (tuple_proj<Int-Int>.1 self)
+        ))"; true
+    }
+
+    #[thrust::predicate]
+    fn step(self, item: Self::Item, dist: Self) -> bool {
+        // self.end == dist.end && self.start == item && self.start + 1 == dist.start
+        // is written as following:
+        "(and
+            (= (tuple_proj<Int-Int>.1 self) (tuple_proj<Int-Int>.1 dist))
+            (= (tuple_proj<Int-Int>.0 self) item)
+            (= (+ (tuple_proj<Int-Int>.0 self) 1) (tuple_proj<Int-Int>.0 dist))
+        )"; true
+    }
+}
+
+fn main() {
+    let mut range = Range {
+        start: 0,
+        end: 5,
+    };
+
+    let mut count = 0;
+    let mut sum = 0;
+    while let Some(i) = range.next() {
+        // assert!(range.start == i + 1 && range.start <= range.end);
+        count += 1;
+        sum += i;
+    }
+
+    assert!(count == 5);
+    // assert!(sum == 10);
+    assert!(range.start > 0);
+}
