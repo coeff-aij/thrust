88beb28c9557070dc01fc4001c990d246674fc44
coeff-aij <175928954+coeff-aij@users.noreply.github.com>
2026-09-25 22:07:42 +0900

    Rewrite Range's predicate bodies in fold_produces_call_law as Rust syntax

--- tests/ui/pass/traits/fold_produces_call_law.rs
+++ tests/ui/pass/traits/fold_produces_call_law.rs
@@ -121,41 +121,26 @@ impl Iterator for Range {
 
     #[thrust_macros::predicate]
     fn invariant(self) -> bool {
-        "true";
         true
     }
 
     #[thrust_macros::predicate]
     fn completed(&mut self) -> bool {
-        "(and
-            (not (<
-                (tuple_proj<Int-Int>.0 (mut_current<Tuple<Int-Int>> self_))
-                (tuple_proj<Int-Int>.1 (mut_current<Tuple<Int-Int>> self_))
-            ))
-            (=  (mut_current<Tuple<Int-Int>> self_) (mut_final<Tuple<Int-Int>> self_))
-        )";
-        true
+        !((*self).start < (*self).end) && *self == !self
     }
 
     #[thrust_macros::predicate]
     fn step(self, item: Self::Item, dist: Self) -> bool {
-        "(and
-            (< (tuple_proj<Int-Int>.0 self_) (tuple_proj<Int-Int>.1 self_))
-            (= (tuple_proj<Int-Int>.1 self_) (tuple_proj<Int-Int>.1 dist))
-            (= (tuple_proj<Int-Int>.0 self_) item)
-            (= (+ (tuple_proj<Int-Int>.0 self_) 1) (tuple_proj<Int-Int>.0 dist))
-        )";
-        true
+        self.start < self.end
+            && self.end == dist.end
+            && self.start == item
+            && self.start + 1 == dist.start
     }
 
     #[thrust_macros::predicate]
     fn produces(self, item: Self::Item) -> bool {
         // self.start <= item && item < self.end
-        "(and
-            (<= (tuple_proj<Int-Int>.0 self_) item)
-            (< item (tuple_proj<Int-Int>.1 self_))
-        )";
-        true
+        self.start <= item && item < self.end
     }
 }
 
